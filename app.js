ad4de0ba1d252e547e55e1debdb36801fb7ee07a
Rahmath-001 <rehamathullah7@gmail.com>
2022-09-08 19:08:01 +0530

    express experiment

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
-import http from "http";
+import express from "express"
+import fs from "fs/promises";
+const app = express();
 
-const port=5000;
+const port = 5000;
 
-const server=http.createServer((req,res)=>{
-    console.log(req.headers);
-    // res.setHeader("Content-Type","application/json")
-    // console.log(req.body);
-    
-    res.end("This is a RAHMATH's HTTP server");
-    // res.statusCode(200).json({"success":"Hello"})
+
+//JSON Body Parser
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).json({ success: "Welcome To the Tasky Application" })
 })
 
-server.listen(port, ()=>{
-    console.log("Server Started at port ",port)
+/*
+METHOD : POST
+API Endpoint : /api/signup
+Body : 
+firstname ; 
+lastname
+phone
+email
+password 
+password2
+address
+*/
+
+
+
+
+
+
+app.post("/api/signup", async (req, res) => {
+    try {
+        // console.log(req.body);
+        let { firstname, lastname, email, password, password2, address, phone } = req.body;
+        let body = req.body;
+
+        // console.log(email);
+        if (!email || !firstname || !lastname || !phone || !address || !password || !password2) {
+            return res.status(400).json({ "error": "Some Fields Are Missing " });
+        }
+        if (password !== password2) {
+            return res.status(400).json({ "error": "Passwords are Not Same" });
+        }
+        let fileData = await fs.readFile("data.json");
+        fileData = JSON.parse(fileData);
+        fileData.push(body);
+        await fs.writeFile("data.json", JSON.stringify(fileData));
+        res.status(200).json({ success: "User Signed Up Succesfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" })
+    }
 })
+
+app.listen(port, () => {
+    console.log("Server Started at Port ", port);
+})
+
