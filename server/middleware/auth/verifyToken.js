69d9299cf0631f2155d0552713d9025af9d45da9
Rahmath-001 <rehamathullah7@gmail.com>
2022-12-24 18:46:00 +0530

    changes made

---
+++ server/middleware/auth/verifyToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import config from "config";
+
+
+let private_key = config.get("PRIVATE_KEY");
+
+function authMiddleware(req, res, next) {
+    try {
+        const token = req.headers['auth-token'];
+        const payload = jwt.verify(token, private_key);
+        console.log(payload)
+        req.payload = payload;
+        return next();
+    } catch (error) {
+        console.error(error);
+        return res.status(401).json({ "error": "Unauthorised Access" });
+    }
+}
+export default authMiddleware;
